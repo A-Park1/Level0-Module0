b70741e27a046cfc2306bc6130e74b616a0a5e87
league <league@imac-22.attlocal.net>
2024-08-01 11:55:27 -0700

    aa

--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -1,15 +1,19 @@
 package _03_print_and_popups._5_awesome_or_not;
 
+import java.awt.Component;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
-
 public class AwesomeOrNot {
 
-	private static final Object whole = null;
+	private static final Object whole = null;	
+	
 
 	// 1. Make a main method that includes everything below
-
+	public static void main(String[] args) {
+		
+		
+	
 	
 		Random ran = new Random();    //This will be used below to make a random number. 
 		
@@ -18,12 +22,13 @@ public class AwesomeOrNot {
 		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
 		
 		// 3. Print your variable to the console
-		JOptionPane.showMessageDialog(null, whole);
-			
-		// 4. Get the user to enter something that they think is awesome
-
-		// 5. If your variable is  0
 	
+		JOptionPane.showMessageDialog(null, count);
+	
+		
+// 4. Get the user to enter something that they think is awesome
+String awesome= JOptionPane.showInputDialog(5);
+		// 5. If your variable is  0
 			// -- tell the user whatever they entered is awesome!
 	
 		// 6. If your variable is  1
@@ -37,5 +42,6 @@ public class AwesomeOrNot {
 		// 8. If your variable is  3
 	
 			// -- invent your own message to give to the user (be nice).
+	
+	}
 }
-
